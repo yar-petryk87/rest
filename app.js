bae72482c0279b45411e185879ad54c1c15e5cb0
Yaroslav Petryk <y.petryk87@ukr.net>
2017-10-02 18:56:48 +0300

    init

--- app.js
+++ app.js
@@ -45,4 +45,9 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+var port = process.env.PORT || 8000;
+app.listen(port, function() {
+console.log("Listening on " + port);
+});
+
 module.exports = app;
